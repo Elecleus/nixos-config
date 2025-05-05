1f9331d2aa1360506f82c90d877bc99c65e249f2
Zephyr Du <elecleus@outlook.com>
2025-05-05 21:00:51 +0800

    remove cosmic-wallpapers overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,15 +1,3 @@
 {
-  nixpkgs.overlays = [
-    (
-      final: prev: {
-        cosmic-wallpapers = prev.cosmic-wallpapers.overrideAttrs {
-          # src = fetchTarball {
-          #   url = ./cosmic-wallpapers-master.tar;
-          #   sha256 = "";
-          # };
-          src = ./cosmic-wallpapers-master.tar;
-        };
-      }
-    )
-  ];
+  nixpkgs.overlays = [];
 }
